4ed876b33be4c4d969129e78348dbc1c69c74011
Yulia Petryk <yulya.petruk.03@gmail.com>
2024-04-21 19:48:46 +0300

    Add 2d-tree method

---
+++ Lab2/range_tree.py
@@ -0,0 +1,11 @@
+
+class Node:
+    def __init__(self, lx, rx, left, right):
+        self.lx = lx
+        self.rx = rx
+        self.left = left
+        self.right = right
+        self.node = None
+        self.points = []
+
+
